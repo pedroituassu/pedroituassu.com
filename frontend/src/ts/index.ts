5948ee8cb8d4c628063cda2e86b92e2039ff81ce
pedroituassu <pedroituassu@proton.me>
2025-02-20 14:12:13 -0400

    updated icons, started to update to light theme compatibility,added typescript, updated backend version
    I changed the icons used by some skills in the front-end so they're compatible to light theme, also started to add some dark: statements to make the website able to be used in light or dark theme. Started to try out typescript, i think it's working but the (probably backend) CORS doesn't let scripts run the API info. Updated backend version to 0.1.0 and will start to use semantic versioning.

---
+++ frontend/src/ts/index.ts
@@ -0,0 +1,17 @@
+async function getData() {
+    const url = "http://localhost:8080/experience";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+
+getData();   
